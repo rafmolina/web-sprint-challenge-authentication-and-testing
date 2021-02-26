9f57e1042a298d902343af5354912ca181adc9de
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-02-26 13:55:32 -0700

    testing errors

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,27 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const request = require('supertest');
+const db = require('../data/dbConfig');
+const server = require('./server');
+
+const sunny = {name:"sunny", username:"moore"}
+
+beforeAll(async ()=>{
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async ()=>{
+  await db("users").truncate()
+})
+
+afterAll(async ()=>{
+  await db.destroy()
+})
+
+describe("[POST] /register", ()=>{
+  it("responds with created user", async ()=>{
+    let res
+    res = await request(server).post("/register").send(sunny)
+    expect(res.body).toMatchObject({id:1, ...sunny})
+  })
 })
